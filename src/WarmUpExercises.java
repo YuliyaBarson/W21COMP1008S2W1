f09425927086d0bd6494e0fe6f0265731d208a79
Yuliya Barson <200468772@student.georgianc.on.ca>
2021-01-21 14:31:17 -0500

    Completed challenges 1 and 2

---
+++ src/WarmUpExercises.java
@@ -0,0 +1,13 @@
+public class WarmUpExercises {
+
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0; i -= 5) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i <= 100; i += 10) {
+            System.out.println(i);
+        }
+    }
+}
+
